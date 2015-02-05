2a630ab5671b2d7bc08aedc461ed68da8ab7854c
Tony Trivison <shambles72@hotmail.com>
2015-02-05 00:05:51 -0800

    Initial commit of Ember project

---
+++ Ember/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ember.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Emberlabs";
+            ViewBag.Tagline = "We are IT consultants who specialize in supporting small businesses. Nice to meet you!";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Ember/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ember.MvcApplication" Language="C#" %>
