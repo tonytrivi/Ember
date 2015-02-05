f8a1f7d3c5ce2295b5dd12f47a1f29cc47792755
Tony Trivison <shambles72@hotmail.com>
2015-02-05 14:50:50 -0800

    Adding content - commented form

--- Ember/Controllers/HomeController.cs
+++ Ember/Controllers/HomeController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ember.Models;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+
 
 namespace Ember.Controllers
 {
@@ -13,6 +19,24 @@ namespace Ember.Controllers
             ViewBag.Title = "Emberlabs";
             ViewBag.Tagline = "We are IT consultants who specialize in supporting small businesses. Nice to meet you!";
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Ember.Models.Contact contact)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    //send a message
+            //    MailMessage msg = new MailMessage(contact.Email, "shambles72@hotmail.com", "You are cool", "You are really cool");
+
+            //    SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+            //    System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("username@domain.com", "yourpassword");
+            //    smtpClient.Credentials = credentials;
+
+            //    smtpClient.Send(mailMsg);
+            //}
+           
+            return View(contact);
         }
 
         public ActionResult About()
---
+++ Ember/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ember.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
